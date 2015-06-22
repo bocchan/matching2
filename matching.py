4e1f01601b263f4a297f27787900beaf1bb03e49
Taiki Iida <track_alien@yahoo.co.jp>
2015-06-22 21:27:26 +0900

    0622のmatchingです

--- matching.py
+++ matching.py
@@ -1,47 +1,37 @@
-import copy
-import numpy as np
-
-m_prefs=np.array([[4,0,1,2,3],[1,2,0,3,4],[3,1,0,2,4]])
-f_prefs=np.array([[0,1,2,3],[1,0,3,2],[1,2,0,3],[0,3,2,1]])
-
-def array_to_dict(array):
-    dict = {}
-    for x, y in enumerate(array):
-        dict[x] = list(y)
-    return dict
-
-  
-def deferred_acceptance(m_prefs,f_prefs):
-    m_prefers = array_to_dict(m_prefs)
-    f_prefers = array_to_dict(f_prefs)
-    guys = sorted(m_prefers.keys())
-    gals = sorted(f_prefers.keys())
-    guysfree = guys[:]
-    engaged  = {}
-    guyprefers2 = copy.deepcopy(m_prefers)
-    galprefers2 = copy.deepcopy(f_prefers)
-    while guysfree:
-        guy = guysfree.pop(0)
-        guyslist = guyprefers2[guy]
-        gal = guyslist.pop(0)
-        fiance = engaged.get(gal)
-        if not fiance:
-            # She's free
-            engaged[gal] = guy
-        else:
-            # The bounder proposes to an engaged lass!
-            galslist = galprefers2[gal]
-            if galslist.index(fiance) > galslist.index(guy):
-                # She prefers new guy
-                engaged[gal] = guy
-                if guyprefers2[fiance]:
-                    # Ex has more girls to try
-                    guysfree.append(fiance)
+def deferred_acceptance(prop_prefs, resp_prefs):
+    
+    prop_num = len(prop_prefs)
+    resp_num = len(resp_prefs)
+    single = range(prop_num)
+    prop_unmatched = resp_num
+    resp_unmatched = prop_num
+    
+    prop_matched = [prop_unmatched for w in range(prop_num)]
+    resp_matched = [resp_unmatched for z in range(resp_num)]
+    
+    counter = [0 for p in range(prop_num)]
+    
+    while len(single)>0:
+        x = single.pop(0)
+        y = prop_prefs[x][counter[x]]
+        if y != prop_unmatched:
+            if resp_matched[y] == resp_unmatched:
+                if resp_prefs[y].index(resp_unmatched) > resp_prefs[y].index(x):
+                    resp_matched[y] = x
+                    prop_matched[x] = y
+                else:
+                    single.insert(0, x)
+                    counter[x] += 1
             else:
-                # She is faithful to old fiance
-                if guyslist:
-                    # Look again
-                    guysfree.append(guy)
-    return engaged
- 
-print engaged #女性、男性の順
+                if resp_prefs[y].index(resp_matched[y]) > resp_prefs[y].index(x):
+                    single.insert(0, resp_matched[y])
+                    prop_matched[resp_matched[y]] = prop_unmatched
+                    resp_matched[y] = x
+                    prop_matched[x] = y
+               
+                else:
+                    single.insert(0, x)
+                    counter[x] += 1
+    
+    
+    return prop_matched, resp_matched
