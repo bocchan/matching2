49b1b554c25f1c841e93a0074b16657528db9aa3
Taiki Iida <track_alien@yahoo.co.jp>
2015-06-27 22:26:07 +0900

    pep8で整形しました

--- matching.py
+++ matching.py
@@ -1,37 +1,36 @@
-﻿def deferred_acceptance(prop_prefs, resp_prefs):
-    
+def deferred_acceptance(prop_prefs, resp_prefs, caps=None):
+
     prop_num = len(prop_prefs)
     resp_num = len(resp_prefs)
-    single = range(prop_num)
+    single = list(range(prop_num))
     prop_unmatched = resp_num
     resp_unmatched = prop_num
-    
-    prop_matched = [prop_unmatched for w in range(prop_num)]
-    resp_matched = [resp_unmatched for z in range(resp_num)]
-    
-    counter = [0 for p in range(prop_num)]
-    
+
+    prop_matched = [prop_unmatched for w in list(range(prop_num))]
+    resp_matched = [resp_unmatched for z in list(range(resp_num))]
+
+    counter = [0 for p in list(range(prop_num))]
+
     while len(single)>0:
         x = single.pop(0)
         y = prop_prefs[x][counter[x]]
         if y != prop_unmatched:
             if resp_matched[y] == resp_unmatched:
-                if resp_prefs[y].index(resp_unmatched) > resp_prefs[y].index(x):
+                if resp_prefs[y].index(resp_unmatched)>resp_prefs[y].index(x):
                     resp_matched[y] = x
                     prop_matched[x] = y
                 else:
                     single.insert(0, x)
                     counter[x] += 1
             else:
-                if resp_prefs[y].index(resp_matched[y]) > resp_prefs[y].index(x):
+                if resp_prefs[y].index(resp_matched[y])>resp_prefs[y].index(x):
                     single.insert(0, resp_matched[y])
                     prop_matched[resp_matched[y]] = prop_unmatched
                     resp_matched[y] = x
                     prop_matched[x] = y
-               
+
                 else:
                     single.insert(0, x)
                     counter[x] += 1
-    
-    
+
     return prop_matched, resp_matched
